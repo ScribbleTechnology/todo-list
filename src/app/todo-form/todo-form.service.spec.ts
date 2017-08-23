7fd03333b22f2571e79866740a457b5a196ba6ae
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2017-08-22 20:59:55 -0500

    Refactor test.

--- src/app/todo-form/todo-form.service.spec.ts
+++ src/app/todo-form/todo-form.service.spec.ts
@@ -15,15 +15,14 @@ describe('TodoFormService', () => {
 
   it('should return task data', inject([TodoFormService], (service: TodoFormService) => {
     service.tasks = [
-      {id: 1, task: 'Get Oil Change'},
-      {id: 2, task: 'Grocery Shopping'},
-      {id: 3, task: 'Create Angular 4 Demo'}
+      {task: 'Get Oil Change'},
+      {task: 'Grocery Shopping'},
+      {task: 'Create Angular 4 Demo'}
     ];
 
     const actual = service.getTasks();
 
     expect(actual.length).toEqual(3);
-    expect(actual[0].id).toEqual(service.tasks[0].id);
     expect(actual[0].task).toEqual(service.tasks[0].task);
   }));
 });
